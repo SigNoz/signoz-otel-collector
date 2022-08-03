6267e57dd93be554043cfbdbab1c9b68644adad8
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-08-03 16:13:44 +0530

    Add top level operations mv (#2073)

---
+++ exporter/clickhousetracesexporter/migrations/000015_top_level_operations_mv.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS signoz_traces.top_level_operations;
+DROP VIEW IF EXISTS signoz_traces.sub_root_operations;
+DROP VIEW IF EXISTS signoz_traces.root_operations;
---
+++ exporter/clickhousetracesexporter/migrations/000015_top_level_operations_mv.up.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS signoz_traces.top_level_operations (
+    name LowCardinality(String) CODEC(ZSTD(1)),
+    serviceName LowCardinality(String) CODEC(ZSTD(1))
+) ENGINE ReplacingMergeTree()
+ORDER BY (serviceName, name);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.sub_root_operations
+TO signoz_traces.top_level_operations
+AS SELECT DISTINCT
+    name,
+    serviceName
+FROM signoz_traces.signoz_index_v2 AS A, signoz_traces.signoz_index_v2 AS B
+WHERE (A.serviceName != B.serviceName) AND (A.parentSpanID = B.spanID);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.root_operations
+TO signoz_traces.top_level_operations
+AS SELECT DISTINCT
+    name,
+    serviceName
+FROM signoz_traces.signoz_index_v2
+WHERE parentSpanID = '';
