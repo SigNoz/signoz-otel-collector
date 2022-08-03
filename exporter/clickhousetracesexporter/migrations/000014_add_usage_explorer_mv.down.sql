a97bbf00eeae7475b3e6f6a1f2e995589cbc9da2
Vishal Sharma <makeavish786@gmail.com>
2022-08-03 15:40:15 +0530

    feat: add usage explorer mv (#2072)
    feat: add usage explorer mv

---
+++ exporter/clickhousetracesexporter/migrations/000014_add_usage_explorer_mv.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS signoz_traces.usage_explorer
+DROP VIEW IF EXISTS signoz_traces.usage_explorer_mv
---
+++ exporter/clickhousetracesexporter/migrations/000014_add_usage_explorer_mv.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS signoz_traces.usage_explorer (
+  timestamp DateTime64(9) CODEC(DoubleDelta, LZ4),
+  service_name LowCardinality(String) CODEC(ZSTD(1)),
+  count UInt64 CODEC(T64, ZSTD(1))
+) ENGINE SummingMergeTree()
+PARTITION BY toDate(timestamp)
+ORDER BY (timestamp, service_name);
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.usage_explorer_mv
+TO signoz_traces.usage_explorer
+AS SELECT
+  toStartOfHour(timestamp) as timestamp,
+  serviceName as service_name,
+  count() as count
+FROM signoz_traces.signoz_index_v2
+GROUP BY timestamp, serviceName;
