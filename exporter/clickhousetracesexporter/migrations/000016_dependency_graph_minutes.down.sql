9f18055ff2d841e724dce4993c9f9f5832f37528
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-08-04 13:03:58 +0530

    feat: add aggregating mvs for dependency graph (#2076)
    * feat: add aggregating mvs for dependency graph

---
+++ exporter/clickhousetracesexporter/migrations/000016_dependency_graph_minutes.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS signoz_traces.dependency_graph_minutes;
+DROP VIEW IF EXISTS signoz_traces.dependency_graph_minutes_service_calls_mv;
+DROP VIEW IF EXISTS signoz_traces.dependency_graph_minutes_db_calls_mv;
+DROP VIEW IF EXISTS signoz_traces.dependency_graph_minutes_messaging_calls_mv;
