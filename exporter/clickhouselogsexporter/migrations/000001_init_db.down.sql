dcc07e0f4b5d9f9fd73186d18ec49dd198f8b2ac
Ankit Nayan <ankit@signoz.io>
2022-09-10 20:15:00 +0530

    feat: distributed schema WIP

--- exporter/clickhouselogsexporter/migrations/000001_init_db.down.sql
+++ exporter/clickhouselogsexporter/migrations/000001_init_db.down.sql
@@ -1,12 +1,2 @@
-DROP TABLE IF EXISTS resource_keys_string_final_mv ON CLUSTER signoz;
-DROP TABLE IF EXISTS atrribute_keys_float64_final_mv ON CLUSTER signoz;
-DROP TABLE IF EXISTS atrribute_keys_int64_final_mv ON CLUSTER signoz;
-DROP TABLE IF EXISTS atrribute_keys_string_final_mv ON CLUSTER signoz;
-DROP TABLE IF EXISTS logs_resource_keys ON CLUSTER signoz;
-DROP TABLE IF EXISTS logs_atrribute_keys ON CLUSTER signoz;
 DROP TABLE IF EXISTS logs ON CLUSTER signoz;
-
-
 DROP TABLE IF EXISTS distributed_logs ON CLUSTER signoz;
-DROP TABLE IF EXISTS distributed_logs_atrribute_keys ON CLUSTER signoz;
-DROP TABLE IF EXISTS distributed_logs_resource_keys ON CLUSTER signoz;
--- exporter/clickhouselogsexporter/migrations/000002_add_minmax_idx.up.sql
+++
@@ -1,3 +0,0 @@
-alter table signoz_logs.logs ON CLUSTER signoz add index id_minmax id TYPE minmax GRANULARITY 1;
-
-alter table signoz_logs.distributed_logs ON CLUSTER signoz add index id_minmax id TYPE minmax GRANULARITY 1;
