a70ed4ffdf48c6f3efaeab1b34e0a79cc164c2ac
Ankit Nayan <ankit@signoz.io>
2022-09-11 18:12:13 +0530

    feat: distributed schema WIP

--- exporter/clickhouselogsexporter/migrations/000001_init_db.down.sql
+++ exporter/clickhouselogsexporter/migrations/000001_init_db.down.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS signoz_logs.logs ON CLUSTER signoz;
 DROP TABLE IF EXISTS signoz_logs.distributed_logs ON CLUSTER signoz;
+
+DROP TABLE IF EXISTS signoz_logs.schema_migrations ON CLUSTER signoz;
+DROP TABLE IF EXISTS signoz_logs.distributed_schema_migrations ON CLUSTER signoz;
