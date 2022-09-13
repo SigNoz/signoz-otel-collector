f73e28cf95dc6d086176af832e516256f4fdbccd
Ankit Nayan <ankit@signoz.io>
2022-09-13 19:34:03 +0530

    chore: adding column to test migration

--- exporter/clickhousetracesexporter/migrations/000017_add_test_column.down.sql
+++ exporter/clickhousetracesexporter/migrations/000017_add_test_column.down.sql
@@ -2,4 +2,4 @@ ALTER TABLE signoz_traces.signoz_index_v2 ON CLUSTER signoz
     DROP COLUMN IF EXISTS `testColumn`;
 
 ALTER TABLE signoz_traces.distributed_signoz_index_v2 ON CLUSTER signoz
-    DROP COLUMN IF EXISTS `testColumn`,
+    DROP COLUMN IF EXISTS `testColumn`;
