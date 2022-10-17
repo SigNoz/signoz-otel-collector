beb017caaac115c1701669503f4e577463054335
Ankit Nayan <ankit@signoz.io>
2022-10-17 19:18:14 +0530

    chore: removing added test column

--- exporter/clickhousetracesexporter/migrations/000017_add_test_column.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE signoz_traces.signoz_index_v2 ON CLUSTER signoz
-    DROP COLUMN IF EXISTS `testColumn`;
-
-ALTER TABLE signoz_traces.distributed_signoz_index_v2 ON CLUSTER signoz
-    DROP COLUMN IF EXISTS `testColumn`;
--- exporter/clickhousetracesexporter/migrations/000017_add_test_column.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE signoz_traces.signoz_index_v2 ON CLUSTER signoz
-    ADD COLUMN IF NOT EXISTS `testColumn` LowCardinality(String) CODEC(ZSTD(1));
-
-ALTER TABLE signoz_traces.distributed_signoz_index_v2 ON CLUSTER signoz
-    ADD COLUMN IF NOT EXISTS `testColumn` LowCardinality(String) CODEC(ZSTD(1));
