4c6d363f58262bb320a2b1f7b1309629103913bc
Ankit Nayan <ankit@signoz.io>
2022-12-02 12:24:37 +0530

    fixes in migration files

--- exporter/clickhouselogsexporter/migrations/000001_init_db.down.sql
+++ exporter/clickhouselogsexporter/migrations/000001_init_db.down.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS signoz_logs.logs ON CLUSTER cluster;
-
-
 DROP TABLE IF EXISTS signoz_logs.logs_atrribute_keys ON CLUSTER cluster;
-
-
 DROP TABLE IF EXISTS signoz_logs.logs_resource_keys ON CLUSTER cluster;
+DROP TABLE IF EXISTS signoz_logs.resource_keys_string_final_mv ON CLUSTER cluster;
+DROP TABLE IF EXISTS signoz_logs.atrribute_keys_float64_final_mv ON CLUSTER cluster;
+DROP TABLE IF EXISTS signoz_logs.atrribute_keys_int64_final_mv ON CLUSTER cluster;
+DROP TABLE IF EXISTS signoz_logs.atrribute_keys_string_final_mv ON CLUSTER cluster;
--- exporter/clickhouselogsexporter/migrations/000003_add_distributed_table.down.sql
+++ exporter/clickhouselogsexporter/migrations/000003_add_distributed_table.down.sql
@@ -1,5 +1,3 @@
 DROP TABLE IF EXISTS signoz_logs.distributed_logs ON CLUSTER cluster;
-DROP TABLE IF EXISTS signoz_logs.schema_migrations ON CLUSTER cluster;
--- DROP TABLE IF EXISTS signoz_logs.distributed_schema_migrations ON CLUSTER cluster;
 DROP TABLE IF EXISTS signoz_logs.distributed_logs_atrribute_keys ON CLUSTER cluster;
 DROP TABLE IF EXISTS signoz_logs.distributed_logs_resource_keys ON CLUSTER cluster;
