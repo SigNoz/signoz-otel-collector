a81d21b96b40f6760c090f02508046175e584d12
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-12-26 19:39:51 +0530

    Update span metrics processor (#47)
    * Update span metrics processor
    
    * update stage for auto enable
    
    * Use the default sanitization

--- processor/signozspanmetricsprocessor/config.go
+++ processor/signozspanmetricsprocessor/config.go
@@ -18,13 +18,23 @@ import (
 	"time"
 
 	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/featuregate"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 )
 
 const (
-	delta      = "AGGREGATION_TEMPORALITY_DELTA"
-	cumulative = "AGGREGATION_TEMPORALITY_CUMULATIVE"
+	delta                  = "AGGREGATION_TEMPORALITY_DELTA"
+	cumulative             = "AGGREGATION_TEMPORALITY_CUMULATIVE"
+	dropSanitizationGateID = "processor.signozspanmetrics.PermissiveLabelSanitization"
 )
+
+func init() {
+	featuregate.GetRegistry().MustRegisterID(
+		dropSanitizationGateID,
+		featuregate.StageAlpha,
+		featuregate.WithRegisterDescription("Controls whether to change labels starting with '_' to 'key_'"),
+	)
+}
 
 // Dimension defines the dimension name and optional default value if the Dimension is missing from a span attribute.
 type Dimension struct {
@@ -49,17 +59,21 @@ type Config struct {
 	// - span.kind
 	// - status.code
 	// The dimensions will be fetched from the span's attributes. Examples of some conventionally used attributes:
-	// https://github.com/open-telemetry/opentelemetry-collector/blob/main/translator/conventions/opentelemetry.go.
+	// https://github.com/open-telemetry/opentelemetry-collector/blob/main/model/semconv/opentelemetry.go.
 	Dimensions []Dimension `mapstructure:"dimensions"`
+
 	// DimensionsCacheSize defines the size of cache for storing Dimensions, which helps to avoid cache memory growing
 	// indefinitely over the lifetime of the collector.
 	// Optional. See defaultDimensionsCacheSize in processor.go for the default value.
 	DimensionsCacheSize int `mapstructure:"dimensions_cache_size"`
 
 	AggregationTemporality string `mapstructure:"aggregation_temporality"`
+
+	// skipSanitizeLabel if enabled, labels that start with _ are not sanitized
+	skipSanitizeLabel bool
 }
 
-// GetAggregationTemporality converts the string value given in the config into a MetricAggregationTemporality.
+// GetAggregationTemporality converts the string value given in the config into a AggregationTemporality.
 // Returns cumulative, unless delta is correctly specified.
 func (c Config) GetAggregationTemporality() pmetric.AggregationTemporality {
 	if c.AggregationTemporality == delta {
