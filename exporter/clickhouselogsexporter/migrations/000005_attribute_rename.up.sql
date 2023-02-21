db3fcce08f58345d5876379c1e548a5c94908d76
nityanandagohain <nityanandagohain@gmail.com>
2023-02-21 18:11:43 +0530

    fix: correct type added for drop command

--- exporter/clickhouselogsexporter/migrations/000005_attribute_rename.up.sql
+++ exporter/clickhouselogsexporter/migrations/000005_attribute_rename.up.sql
@@ -1,10 +1,10 @@
-RENAME TABLE if exists signoz_logs.logs_atrribute_keys TO signoz_logs.logs_attribute_keys;
+RENAME TABLE IF EXISTS signoz_logs.logs_atrribute_keys TO signoz_logs.logs_attribute_keys;
 
 
 -- Materialized view
-DROP table if exists atrribute_keys_string_final_mv;
-DROP table if exists atrribute_keys_int64_final_mv;
-DROP table if exists atrribute_keys_float64_final_mv;
+DROP VIEW IF EXISTS atrribute_keys_string_final_mv;
+DROP VIEW IF EXISTS atrribute_keys_int64_final_mv;
+DROP VIEW IF EXISTS atrribute_keys_float64_final_mv;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS  attribute_keys_string_final_mv ON CLUSTER cluster TO signoz_logs.logs_attribute_keys AS
 SELECT
@@ -26,7 +26,7 @@ ORDER BY  name;
 
 
 -- Distributed table
-DROP table if exists signoz_logs.distributed_logs_atrribute_keys ;
+DROP TABLE IF EXISTS signoz_logs.distributed_logs_atrribute_keys ;
 
 CREATE TABLE IF NOT EXISTS signoz_logs.distributed_logs_attribute_keys  ON CLUSTER cluster AS signoz_logs.logs_attribute_keys
 ENGINE = Distributed("cluster", "signoz_logs", logs_attribute_keys, cityHash64(datatype));
