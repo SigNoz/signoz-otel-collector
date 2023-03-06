4b9b3ea2032cb7b633faff7e0a76e432f598cb56
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2023-03-07 01:44:51 +0530

    Add service implementation (#58)

--- service/service.go
+++ service/service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/SigNoz/signoz-otel-collector/opamp"
+	"github.com/SigNoz/signoz-otel-collector/signozcol"
 	"go.uber.org/zap"
 )
 
@@ -24,12 +26,47 @@ type service struct {
 	client opamp.Client
 }
 
+func New(wrappedCollector *signozcol.WrappedCollector, logger *zap.Logger, managerConfigPath string, collectorConfigPath string) (*service, error) {
+
+	var client opamp.Client
+	var err error
+
+	// Running without Opamp
+	if managerConfigPath == "" {
+		client = opamp.NewSimpleClient(wrappedCollector)
+	} else {
+		managerConfig, err := opamp.ParseAgentManagerConfig(managerConfigPath)
+		// Invalid config file
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse manager config: %w", err)
+		}
+		serverClientOpts := &opamp.NewServerClientOpts{
+			Logger:             logger,
+			Config:             managerConfig,
+			WrappedCollector:   wrappedCollector,
+			CollectorConfgPath: collectorConfigPath,
+		}
+		client, err = opamp.NewServerClient(serverClientOpts)
+	}
+
+	return &service{
+		client: client,
+		l:      logger,
+	}, err
+}
+
 // Start starts the Opamp connection and collector
 func (s *service) Start(ctx context.Context) error {
+	if err := s.client.Start(ctx); err != nil {
+		return fmt.Errorf("failed to start : %w", err)
+	}
 	return nil
 }
 
 // Shutdown stops the Opamp connection and collector
 func (s *service) Shutdown(ctx context.Context) error {
+	if err := s.client.Stop(ctx); err != nil {
+		return fmt.Errorf("failed to stop: %w", err)
+	}
 	return nil
 }
