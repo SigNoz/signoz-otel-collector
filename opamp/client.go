319075519cb7508ac9b20c905905b122dbe6938b
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2023-03-07 01:18:19 +0530

    Add simple client implementation (#51)

--- opamp/client.go
+++ opamp/client.go
@@ -6,4 +6,6 @@ type Client interface {
 	Start(ctx context.Context) error
 
 	Stop(ctx context.Context) error
+
+	Error() error
 }
---
+++ opamp/simple_client_test.go
@@ -0,0 +1,115 @@
+package opamp
+
+import (
+	"context"
+	"testing"
+
+	"github.com/SigNoz/signoz-otel-collector/signozcol"
+	"go.opentelemetry.io/collector/service"
+	"go.uber.org/zap"
+)
+
+func TestNopClientWithCollector(t *testing.T) {
+	coll := signozcol.New(signozcol.WrappedCollectorSettings{
+		ConfigPaths: []string{"testdata/simple/config.yaml"},
+		Version:     "0.0.1",
+		Desc:        "test",
+		LoggingOpts: []zap.Option{zap.AddStacktrace(zap.ErrorLevel)},
+	})
+
+	client := NewSimpleClient(coll)
+
+	err := client.Start(context.Background())
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if coll.GetState() != service.StateRunning {
+		t.Errorf("expected collector to be run")
+	}
+
+	err = client.Stop(context.Background())
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestNopClientWithCollectorError(t *testing.T) {
+	coll := signozcol.New(signozcol.WrappedCollectorSettings{
+		ConfigPaths: []string{"testdata/invalid.yaml"},
+		Version:     "0.0.1",
+		Desc:        "test",
+		LoggingOpts: []zap.Option{zap.AddStacktrace(zap.ErrorLevel)},
+	})
+
+	client := NewSimpleClient(coll)
+
+	err := client.Start(context.Background())
+	if err == nil {
+		t.Errorf("expected error")
+	}
+
+	if coll.GetState() != service.StateClosed {
+		t.Errorf("expected collector to be in closed state")
+	}
+
+	err = client.Stop(context.Background())
+	if err == nil {
+		t.Errorf("expected error")
+	}
+}
+
+func TestNopClientWithCollectorErrorRead(t *testing.T) {
+	coll := signozcol.New(signozcol.WrappedCollectorSettings{
+		ConfigPaths: []string{"testdata/invalid.yaml"},
+		Version:     "0.0.1",
+		Desc:        "test",
+		LoggingOpts: []zap.Option{zap.AddStacktrace(zap.ErrorLevel)},
+	})
+
+	client := NewSimpleClient(coll)
+
+	err := client.Start(context.Background())
+	if err == nil {
+		t.Errorf("expected error")
+	}
+
+	if coll.GetState() != service.StateClosed {
+		t.Errorf("expected collector to be in closed state")
+	}
+
+	err = client.Error()
+	if err == nil {
+		t.Errorf("expected error")
+	}
+}
+
+func TestNopClientWithCollectorErrorChanMultipleTime(t *testing.T) {
+	coll := signozcol.New(signozcol.WrappedCollectorSettings{
+		ConfigPaths: []string{"testdata/invalid.yaml"},
+		Version:     "0.0.1",
+		Desc:        "test",
+		LoggingOpts: []zap.Option{zap.AddStacktrace(zap.ErrorLevel)},
+	})
+
+	client := NewSimpleClient(coll)
+
+	err := client.Start(context.Background())
+	if err == nil {
+		t.Errorf("expected error")
+	}
+
+	if coll.GetState() != service.StateClosed {
+		t.Errorf("expected collector to be in closed state")
+	}
+
+	err = client.Error()
+	if err == nil {
+		t.Errorf("expected error")
+	}
+
+	err = client.Error()
+	if err != nil {
+		t.Errorf("expected no error")
+	}
+}
