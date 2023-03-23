b2d9c83e215cee9c445f4bc59ba38feade1bfca7
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2023-03-23 07:03:52 +0530

    update migration scripts

--- exporter/clickhousetracesexporter/migrations/000020_resource_attributes.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE signoz_traces.signoz_index_v2 ON CLUSTER cluster
-    ADD COLUMN IF NOT EXISTS `resourceTagsMap` Map(LowCardinality(String), String) CODEC(ZSTD(1));
-
-ALTER TABLE signoz_traces.distributed_signoz_index_v2 ON CLUSTER cluster
-    ADD COLUMN IF NOT EXISTS `resourceTagsMap` Map(LowCardinality(String), String) CODEC(ZSTD(1));
--- exporter/clickhousetracesexporter/migrations/000022_dependency_graph_minutes_v2.down.sql
+++ exporter/clickhousetracesexporter/migrations/000022_dependency_graph_minutes_v2.down.sql
@@ -3,4 +3,4 @@ DROP VIEW IF EXISTS signoz_traces.dependency_graph_minutes_service_calls_mv_v2 O
 DROP VIEW IF EXISTS signoz_traces.dependency_graph_minutes_db_calls_mv_v2 ON CLUSTER cluster;
 DROP VIEW IF EXISTS signoz_traces.dependency_graph_minutes_messaging_calls_mv_v2 ON CLUSTER cluster;
 
-DROP TABLE IF EXISTS signoz_traces.distributed_dependency_graph_minutes_v2 ON CLUSTER cluster
+DROP TABLE IF EXISTS signoz_traces.distributed_dependency_graph_minutes_v2 ON CLUSTER cluster;
