adea2a8ac794a2dba3c4e9cebefa65dfe2815a85
makeavish <makeavish786@gmail.com>
2023-04-05 17:32:14 +0530

    chore: update distributed key

--- exporter/clickhousetracesexporter/migrations/000023_span_attributes_mv.up.sql
+++ exporter/clickhousetracesexporter/migrations/000023_span_attributes_mv.up.sql
@@ -11,4 +11,4 @@ TTL toDateTime(timestamp) + INTERVAL 86400 SECOND DELETE
 SETTINGS ttl_only_drop_parts = 1, allow_nullable_key = 1;
 
 CREATE TABLE IF NOT EXISTS signoz_traces.distributed_span_attributes ON CLUSTER cluster AS signoz_traces.span_attributes
-ENGINE = Distributed("cluster", "signoz_traces", span_attributes, cityHash64(tagKey));
+ENGINE = Distributed("cluster", "signoz_traces", span_attributes, cityHash64(rand()));
