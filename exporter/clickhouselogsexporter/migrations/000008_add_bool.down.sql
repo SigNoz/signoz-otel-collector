08a6da0871387b5e7631d2ebffb71dd93fcf21ea
nityanandagohain <nityanandagohain@gmail.com>
2023-10-06 10:34:34 +0530

    fix: migrations updated

--- exporter/clickhouselogsexporter/migrations/000008_add_bool.down.sql
+++ exporter/clickhouselogsexporter/migrations/000008_add_bool.down.sql
@@ -6,3 +6,5 @@ ALTER TABLE signoz_logs.logs ON CLUSTER cluster DROP column IF NOT EXISTS attrib
 ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster DROP column IF NOT EXISTS attributes_bool_key Array(String) CODEC(ZSTD(1));
 
 ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster DROP column IF NOT EXISTS attributes_bool_value Array(Bool) CODEC(ZSTD(1));
+
+DROP TABLE IF EXISTS signoz_logs.attribute_keys_bool_final_mv ON CLUSTER cluster;
--- exporter/clickhouselogsexporter/migrations/000008_add_bool.up.sql
+++ exporter/clickhouselogsexporter/migrations/000008_add_bool.up.sql
@@ -5,3 +5,9 @@ ALTER TABLE signoz_logs.logs ON CLUSTER cluster add column IF NOT EXISTS attribu
 ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster add column IF NOT EXISTS attributes_bool_key Array(String) CODEC(ZSTD(1));
 
 ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster add column IF NOT EXISTS attributes_bool_value Array(Bool) CODEC(ZSTD(1));
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS  attribute_keys_bool_final_mv ON CLUSTER cluster TO signoz_logs.logs_attribute_keys AS
+SELECT
+distinct arrayJoin(attributes_bool_key) as name, 'Bool' datatype
+FROM signoz_logs.logs
+ORDER BY name;
