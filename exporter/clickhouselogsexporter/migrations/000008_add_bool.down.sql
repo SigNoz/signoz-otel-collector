ef226a93839710e4c9f9bd1a25dd61d58fdfe5da
nityanandagohain <nityanandagohain@gmail.com>
2023-10-06 16:24:33 +0530

    fix: migration updated

--- exporter/clickhouselogsexporter/migrations/000008_add_bool.down.sql
+++ exporter/clickhouselogsexporter/migrations/000008_add_bool.down.sql
@@ -1,10 +1,10 @@
 
-ALTER TABLE signoz_logs.logs ON CLUSTER cluster DROP column IF NOT EXISTS attributes_bool_key Array(String) CODEC(ZSTD(1));
+ALTER TABLE signoz_logs.logs ON CLUSTER cluster DROP column IF EXISTS attributes_bool_key;
 
-ALTER TABLE signoz_logs.logs ON CLUSTER cluster DROP column IF NOT EXISTS attributes_bool_value Array(Bool) CODEC(ZSTD(1));
+ALTER TABLE signoz_logs.logs ON CLUSTER cluster DROP column IF EXISTS attributes_bool_value;
 
-ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster DROP column IF NOT EXISTS attributes_bool_key Array(String) CODEC(ZSTD(1));
+ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster DROP column IF EXISTS attributes_bool_key;
 
-ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster DROP column IF NOT EXISTS attributes_bool_value Array(Bool) CODEC(ZSTD(1));
+ALTER TABLE signoz_logs.distributed_logs ON CLUSTER cluster DROP column IF EXISTS attributes_bool_value;
 
 DROP TABLE IF EXISTS signoz_logs.attribute_keys_bool_final_mv ON CLUSTER cluster;
