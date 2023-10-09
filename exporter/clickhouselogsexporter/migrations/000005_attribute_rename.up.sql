b465924bab61eb98f0d606b9af6f807e689edd9d
nityanandagohain <nityanandagohain@gmail.com>
2023-10-09 13:54:01 +0530

    fix: migration reapply issue fixed for logs

--- exporter/clickhouselogsexporter/migrations/000005_attribute_rename.up.sql
+++ exporter/clickhouselogsexporter/migrations/000005_attribute_rename.up.sql
@@ -1,3 +1,6 @@
+-- We are dropping logs_attribute_keys as we are renaming logs_atrribute_keys in the next line
+DROP TABLE IF EXISTS signoz_logs.logs_attribute_keys on CLUSTER cluster;
+
 RENAME TABLE IF EXISTS signoz_logs.logs_atrribute_keys TO signoz_logs.logs_attribute_keys on CLUSTER cluster;
 
 
