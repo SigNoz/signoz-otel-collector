be77ec7b391ddca24f957a21984c646f1db4c460
Nityananda Gohain <nityanandagohain@gmail.com>
2023-10-30 21:16:55 +0530

    Update migrationmanager/migrators/logs/migrations/000008_add_bool.up.sql
    Co-authored-by: Srikanth Chekuri <srikanth.chekuri92@gmail.com>

--- migrationmanager/migrators/logs/migrations/000008_add_bool.up.sql
+++ migrationmanager/migrators/logs/migrations/000008_add_bool.up.sql
@@ -6,7 +6,7 @@ ALTER TABLE signoz_logs.distributed_logs ON CLUSTER {{.SIGNOZ_CLUSTER}} add colu
 
 ALTER TABLE signoz_logs.distributed_logs ON CLUSTER {{.SIGNOZ_CLUSTER}} add column IF NOT EXISTS attributes_bool_value Array(Bool) CODEC(ZSTD(1));
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS  attribute_keys_bool_final_mv ON CLUSTER {{.SIGNOZ_CLUSTER}} TO signoz_logs.logs_attribute_keys AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS  signoz_logs.attribute_keys_bool_final_mv ON CLUSTER {{.SIGNOZ_CLUSTER}} TO signoz_logs.logs_attribute_keys AS
 SELECT
 distinct arrayJoin(attributes_bool_key) as name, 'Bool' datatype
 FROM signoz_logs.logs
