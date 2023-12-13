1ff5fb9c6910e4010e272961b52d3a98dcacb7ae
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2023-12-13 14:13:33 +0530

    Add ttl_only_drop_parts setting

--- migrationmanager/migrators/metrics/migrations/000003_ts_v4.up.sql
+++ migrationmanager/migrators/metrics/migrations/000003_ts_v4.up.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS signoz_metrics.time_series_v4 ON CLUSTER {{.SIGNOZ_CL
 ENGINE = ReplacingMergeTree
         PARTITION BY toDate(unix_milli / 1000)
         ORDER BY (env, temporality, metric_name, fingerprint, unix_milli)
-        TTL toDateTime(unix_milli/1000) + INTERVAL 2592000 SECOND DELETE;
+        TTL toDateTime(unix_milli/1000) + INTERVAL 2592000 SECOND DELETE
+        SETTINGS ttl_only_drop_parts = 1;
 
 CREATE TABLE IF NOT EXISTS signoz_metrics.distributed_time_series_v4 ON CLUSTER {{.SIGNOZ_CLUSTER}} AS signoz_metrics.time_series_v4 ENGINE = Distributed("{{.SIGNOZ_CLUSTER}}", signoz_metrics, time_series_v4, cityHash64(env, temporality, metric_name, fingerprint, unix_milli));
