63365e2ea834111b0bdcd10cd96d205e495a165a
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2024-01-05 17:40:28 +0530

    fix test

--- exporter/clickhousemetricsexporter/config_test.go
+++ exporter/clickhousemetricsexporter/config_test.go
@@ -83,7 +83,7 @@ func Test_loadConfig(t *testing.T) {
 			},
 			ResourceToTelemetrySettings: resourcetotelemetry.Settings{Enabled: true},
 			WatcherInterval:             30 * time.Second,
-			WriteTSToV4:                 false,
+			WriteTSToV4:                 true,
 		})
 }
 
