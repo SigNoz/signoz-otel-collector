528c86ceff9ccf2aab7566a546aa6dd0651b0ecd
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2024-01-10 01:49:31 +0530

    Add column to distributed table as well

--- migrationmanager/migrators/traces/migrations/000025_add_time_column_ttl_top_level.down.sql
+++ migrationmanager/migrators/traces/migrations/000025_add_time_column_ttl_top_level.down.sql
@@ -3,3 +3,6 @@ ALTER TABLE signoz_traces.top_level_operations ON CLUSTER {{.SIGNOZ_CLUSTER}}
 
 ALTER TABLE signoz_traces.top_level_operations ON CLUSTER {{.SIGNOZ_CLUSTER}} 
     REMOVE TTL;
+
+ALTER TABLE signoz_traces.distributed_top_level_operations ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    DROP COLUMN IF EXISTS time;
--- migrationmanager/migrators/traces/migrations/000025_add_time_column_ttl_top_level.up.sql
+++ migrationmanager/migrators/traces/migrations/000025_add_time_column_ttl_top_level.up.sql
@@ -3,3 +3,6 @@ ALTER TABLE signoz_traces.top_level_operations ON CLUSTER {{.SIGNOZ_CLUSTER}}
 
 ALTER TABLE signoz_traces.top_level_operations ON CLUSTER {{.SIGNOZ_CLUSTER}} 
     MODIFY TTL time + INTERVAL 1 MONTH;
+
+ALTER TABLE signoz_traces.distributed_top_level_operations ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    ADD COLUMN IF NOT EXISTS time DateTime DEFAULT now() CODEC(ZSTD(1));
