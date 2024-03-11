54888f1739a413ef355f56f1c4e0d8370758c36c
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2024-03-11 15:43:18 +0530

    Fix migrations

--- migrationmanager/migrators/traces/migrations/000027_add_is_remote_span.down.sql
+++ migrationmanager/migrators/traces/migrations/000027_add_is_remote_span.down.sql
@@ -3,6 +3,15 @@ DROP TABLE IF EXISTS signoz_traces.durationSortMV ON CLUSTER {{.SIGNOZ_CLUSTER}}
 ALTER TABLE signoz_traces.signoz_index_v2 ON CLUSTER {{.SIGNOZ_CLUSTER}}
     DROP COLUMN IF EXISTS isRemote;
 
+ALTER TABLE signoz_traces.distributed_signoz_index_v2 ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    DROP COLUMN IF EXISTS isRemote;
+
+ALTER TABLE signoz_traces.durationSort ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    DROP COLUMN IF EXISTS isRemote;
+
+ALTER TABLE signoz_traces.distributed_durationSort ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    DROP COLUMN IF EXISTS isRemote;
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.durationSortMV ON CLUSTER {{.SIGNOZ_CLUSTER}}
 TO signoz_traces.durationSort
 AS SELECT
@@ -24,7 +33,14 @@ AS SELECT
   gRPCMethod,
   gRPCCode,
   hasError,
-  tagMap
+  tagMap,
+  rpcSystem,
+  rpcService,
+  rpcMethod,
+  responseStatusCode,
+  stringTagMap,
+  numberTagMap,
+  boolTagMap
 FROM signoz_traces.signoz_index_v2
 ORDER BY durationNano, timestamp;
 
--- migrationmanager/migrators/traces/migrations/000027_add_is_remote_span.up.sql
+++ migrationmanager/migrators/traces/migrations/000027_add_is_remote_span.up.sql
@@ -3,7 +3,13 @@ DROP TABLE IF EXISTS signoz_traces.durationSortMV ON CLUSTER {{.SIGNOZ_CLUSTER}}
 ALTER TABLE signoz_traces.signoz_index_v2 ON CLUSTER {{.SIGNOZ_CLUSTER}}
     ADD COLUMN IF NOT EXISTS `isRemote` LowCardinality(String) CODEC(ZSTD(1));
 
+ALTER TABLE signoz_traces.distributed_signoz_index_v2 ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    ADD COLUMN IF NOT EXISTS `isRemote` LowCardinality(String) CODEC(ZSTD(1));
+
 ALTER TABLE signoz_traces.durationSort ON CLUSTER {{.SIGNOZ_CLUSTER}}
+    ADD COLUMN IF NOT EXISTS `isRemote` LowCardinality(String) CODEC(ZSTD(1));
+
+ALTER TABLE signoz_traces.distributed_durationSort ON CLUSTER {{.SIGNOZ_CLUSTER}}
     ADD COLUMN IF NOT EXISTS `isRemote` LowCardinality(String) CODEC(ZSTD(1));
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.durationSortMV ON CLUSTER {{.SIGNOZ_CLUSTER}}
@@ -32,6 +38,9 @@ AS SELECT
   rpcService,
   rpcMethod,
   responseStatusCode,
+  stringTagMap,
+  numberTagMap,
+  boolTagMap,
   isRemote
 FROM signoz_traces.signoz_index_v2
 ORDER BY durationNano, timestamp;
