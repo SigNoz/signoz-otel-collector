ce53d5235f610e783336b99a905931a7a7dbcdfd
grandwizard28 <vibhupandey28@gmail.com>
2024-03-21 15:00:42 +0530

    (feat) return a default topic when tenant_id is not present

--- exporter/signozkafkaexporter/utils.go
+++ exporter/signozkafkaexporter/utils.go
@@ -1,17 +1,20 @@
 package signozkafkaexporter
 
 import (
-	"fmt"
-
 	"go.opentelemetry.io/collector/client"
+)
+
+const (
+	DefaultKafkaTopic = "default"
 )
 
 // getKafkaTopicFromClientMetadata returns the kafka topic from client metadata
 func getKafkaTopicFromClientMetadata(md client.Metadata) (string, error) {
 	// return default topic if no tenant id is found in client metadata
 	signozTenantId := md.Get("signoz_tenant_id")
-	if len(signozTenantId) == 0 {
-		return "", fmt.Errorf("signoz_tenant_id not found in client metadata")
+	if len(signozTenantId) != 0 {
+		return signozTenantId[0], nil
 	}
-	return signozTenantId[0], nil
+
+	return DefaultKafkaTopic, nil
 }
