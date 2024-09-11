ec5b04070e51dbcf36b94b4a6b227a2572d506f8
Raj <rkssisodiya@gmail.com>
2024-09-11 10:08:55 +0530

    chore: some more cleanup

--- processor/signozlogspipelineprocessor/config.go
+++ processor/signozlogspipelineprocessor/config.go
@@ -9,7 +9,6 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/adapter"
 )
 
-// Config defines configuration for Resource processor.
 type Config struct {
 	adapter.BaseConfig `mapstructure:",squash"`
 }
@@ -18,7 +17,7 @@ var _ component.Config = (*Config)(nil)
 
 func (cfg *Config) Validate() error {
 	if len(cfg.BaseConfig.Operators) == 0 {
-		return errors.New("no operators were configured for this logs transform processor")
+		return errors.New("no operators were configured for signozlogspipeline processor")
 	}
 	return nil
 }
--- processor/signozlogspipelineprocessor/factory.go
+++ processor/signozlogspipelineprocessor/factory.go
@@ -1,4 +1,4 @@
-// Brought in as is from logstransform processor in opentelemetry-collector-contrib
+// Brought in as is from signozlogspipeline processor in opentelemetry-collector-contrib
 // with identifiers changed for the new processor
 package signozlogspipelineprocessor
 
@@ -14,7 +14,6 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator"
 )
 
-// NewFactory returns a new factory for the Logs Transform processor.
 func NewFactory() processor.Factory {
 	return processor.NewFactory(
 		component.MustNewType("signozlogspipeline"),
@@ -22,7 +21,7 @@ func NewFactory() processor.Factory {
 		processor.WithLogs(createLogsProcessor, component.StabilityLevelDevelopment))
 }
 
-// Note: This isn't a valid configuration because the processor would do no work.
+// Note: This isn't a valid configuration (no operators would lead to no work being done)
 func createDefaultConfig() component.Config {
 	return &Config{
 		BaseConfig: adapter.BaseConfig{
@@ -38,11 +37,11 @@ func createLogsProcessor(
 	nextConsumer consumer.Logs) (processor.Logs, error) {
 	pCfg, ok := cfg.(*Config)
 	if !ok {
-		return nil, errors.New("could not initialize logs transform processor")
+		return nil, errors.New("could not initialize signozlogspipeline processor")
 	}
 
 	if len(pCfg.BaseConfig.Operators) == 0 {
-		return nil, errors.New("no operators were configured for this logs transform processor")
+		return nil, errors.New("no operators were configured for this signozlogspipeline processor")
 	}
 
 	return newProcessor(pCfg, nextConsumer, set.TelemetrySettings)
