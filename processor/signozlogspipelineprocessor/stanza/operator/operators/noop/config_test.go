e4454792495ef289834fe3d2fa7aeef54f69caac
Raj <rkssisodiya@gmail.com>
2024-09-16 15:48:18 +0530

    feat: bring in noop operator

---
+++ processor/signozlogspipelineprocessor/stanza/operator/operators/noop/config_test.go
@@ -0,0 +1,27 @@
+// Brought in as is from opentelemetry-collector-contrib
+
+package noop
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenttest"
+)
+
+func TestBuildValid(t *testing.T) {
+	cfg := NewConfigWithID("test")
+	set := componenttest.NewNopTelemetrySettings()
+	op, err := cfg.Build(set)
+	require.NoError(t, err)
+	require.IsType(t, &Transformer{}, op)
+}
+
+func TestBuildInvalid(t *testing.T) {
+	cfg := NewConfigWithID("test")
+	set := componenttest.NewNopTelemetrySettings()
+	set.Logger = nil
+	_, err := cfg.Build(set)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "build context is missing a logger")
+}
