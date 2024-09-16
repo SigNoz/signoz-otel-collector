d0181ea331cf2c71c1091eb6a4d9c19628442447
Raj <rkssisodiya@gmail.com>
2024-09-16 15:35:22 +0530

    feat: bring in severity parser operator

--- processor/signozlogspipelineprocessor/stanza/adapter/register_ops.go
+++ processor/signozlogspipelineprocessor/stanza/adapter/register_ops.go
@@ -8,5 +8,6 @@ import (
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/move"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/regex"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/remove"
+	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/severity"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/time"
 )
