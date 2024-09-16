7c2ace9faf0404a905144fbfdc0ce5866969c035
Raj <rkssisodiya@gmail.com>
2024-09-16 14:11:37 +0530

    feat: bring in remove operator

--- processor/signozlogspipelineprocessor/stanza/adapter/register_ops.go
+++ processor/signozlogspipelineprocessor/stanza/adapter/register_ops.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/add"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/copy"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/move"
+	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/remove"
 )
