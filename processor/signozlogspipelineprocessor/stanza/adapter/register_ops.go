3e12830ab9c7cbb06c5d676a667d95b6089305c7
Raj <rkssisodiya@gmail.com>
2024-09-16 14:18:10 +0530

    feat: bring in regex operator

--- processor/signozlogspipelineprocessor/stanza/adapter/register_ops.go
+++ processor/signozlogspipelineprocessor/stanza/adapter/register_ops.go
@@ -5,5 +5,6 @@ import (
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/add"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/copy"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/move"
+	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/regex"
 	_ "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/operators/remove"
 )
