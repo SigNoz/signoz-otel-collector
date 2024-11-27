d60e4466686595aecb5f05cd0e4da623b3075c65
nityanandagohain <nityanandagohain@gmail.com>
2024-11-27 23:41:47 +0530

    fix: skip span

--- exporter/clickhousetracesexporter/writer.go
+++ exporter/clickhousetracesexporter/writer.go
@@ -201,7 +201,8 @@ func (w *SpanWriter) writeModelBatch(ctx context.Context, batchSpans []*Span) er
 		if err != nil {
 			w.logger.Error("could not marshal trace model: ", zap.Error(err))
 			w.logger.Error("trace model: ", zap.Any("trace model", span.TraceModel))
-			return err
+			continue
+			// return err
 		}
 		serializedUsage, err := json.Marshal(usageMap)
 
