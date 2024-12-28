9f3c62339f5f007a677db88a4e421c0df36bd72d
grandwizard28 <vibhupandey28@gmail.com>
2024-12-28 18:00:08 +0530

    docs(metering): add docs for metering

--- pkg/metering/meter.go
+++ pkg/metering/meter.go
@@ -7,16 +7,31 @@ import (
 )
 
 // Meter is an interface that receives telemetry data and
-// calculates billable metrics
+// calculates billable metrics.
 type Meter[T ptrace.Traces | pmetric.Metrics | plog.Logs] interface {
+	// Calculates size of the telemetry data in bytes.
 	Size(T) int
+	// Calculates count of the telemetry data.
 	Count(T) int
 }
 
+// Sizer is an interface that calculates the size of different
+// data structures
+type Sizer interface {
+	SizeOfMapStringAny(map[string]any) int
+}
+
+// Calculates billable metrics for logs.
 type Logs interface {
 	Meter[plog.Logs]
 }
 
-type Sizer interface {
-	SizeOfMapStringAny(map[string]any) int
+// Calculates billable metrics for traces.
+type Traces interface {
+	Meter[plog.Logs]
+}
+
+// Calculates billable metrics for metrics.
+type Metrics interface {
+	Meter[plog.Logs]
 }
