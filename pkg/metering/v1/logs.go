873cdb194d6985f53e948e0608fe91e91d13d481
shivanshu <shivanshu1333@gmail.com>
2024-12-30 10:34:25 +0530

    feat: create a payload meter

--- pkg/metering/v1/logs.go
+++ pkg/metering/v1/logs.go
@@ -1,7 +1,7 @@
 package v1
 
 import (
-	"github.com/SigNoz/signoz-otel-collector/pkg/metering"
+	"github.com/signoz/signoz-otel-collector/pkg/metering"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.uber.org/zap"
 )
