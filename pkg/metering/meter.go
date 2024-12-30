f22b4655e4e58559f9322194aa1540f1bbd4297f
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2024-12-30 17:13:58 +0530

    feat: making metering a separate module (#498)
    Creates a separate submodule
    `github.com/SigNoz/signoz-otel-collector/pkg/metering` and
    `github.com/SigNoz/signoz-otel-collector/pkg/pdatagen` to avoid pulling
    all the dependencies from `signoz-otel-collector` while importing it in
    gateway.

--- pkg/metering/meter.go
+++ pkg/metering/meter.go
@@ -9,29 +9,28 @@ import (
 // Meter is an interface that receives telemetry data and
 // calculates billable metrics.
 type Meter[T ptrace.Traces | pmetric.Metrics | plog.Logs] interface {
-	// Calculates size of the telemetry data in bytes.
+	// Size calculates size of the telemetry data in bytes.
 	Size(T) int
-	// Calculates count of the telemetry data.
+	// Count calculates count of the telemetry data.
 	Count(T) int
 }
 
-// Sizer is an interface that calculates the size of different
-// data structures
+// Sizer is an interface that calculates the size of different of map[string]any
 type Sizer interface {
 	SizeOfMapStringAny(map[string]any) int
 }
 
-// Calculates billable metrics for logs.
+// Logs calculates billable metrics for logs.
 type Logs interface {
 	Meter[plog.Logs]
 }
 
-// Calculates billable metrics for traces.
+// Traces calculates billable metrics for traces.
 type Traces interface {
 	Meter[ptrace.Traces]
 }
 
-// Calculates billable metrics for metrics.
+// Metrics calculates billable metrics for metrics.
 type Metrics interface {
 	Meter[pmetric.Metrics]
 }
