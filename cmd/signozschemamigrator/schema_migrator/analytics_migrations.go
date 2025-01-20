384769e447df5c74d1d98d58780f4c029a1f84a8
nityanandagohain <nityanandagohain@gmail.com>
2025-01-20 16:15:18 +0530

    fix: use correct types

--- cmd/signozschemamigrator/schema_migrator/analytics_migrations.go
+++ cmd/signozschemamigrator/schema_migrator/analytics_migrations.go
@@ -1,7 +1,5 @@
 package schemamigrator
 
-// database := "CREATE DATABASE IF NOT EXISTS signoz_analytics ON CLUSTER %s"
-
 var AnalyticsMigrations = []SchemaMigrationRecord{
 	{
 		MigrationID: 1,
@@ -11,16 +9,16 @@ var AnalyticsMigrations = []SchemaMigrationRecord{
 				Table:    "rule_state_history_v0",
 				Columns: []Column{
 					{Name: "_retention_days", Type: ColumnTypeUInt32, Default: "180"},
-					{Name: "rule_id", Type: ColumnTypeString},
-					{Name: "rule_name", Type: ColumnTypeString},
-					{Name: "overall_state", Type: ColumnTypeString},
+					{Name: "rule_id", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
+					{Name: "rule_name", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
+					{Name: "overall_state", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
 					{Name: "overall_state_changed", Type: ColumnTypeBool},
-					{Name: "state", Type: ColumnTypeString},
+					{Name: "state", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
 					{Name: "state_changed", Type: ColumnTypeBool},
-					{Name: "unix_milli", Type: ColumnTypeInt64},
-					{Name: "fingerprint", Type: ColumnTypeUInt64},
-					{Name: "value", Type: ColumnTypeFloat64},
-					{Name: "labels", Type: ColumnTypeString},
+					{Name: "unix_milli", Type: ColumnTypeInt64, Codec: "Delta(8), ZSTD(1)"},
+					{Name: "fingerprint", Type: ColumnTypeUInt64, Codec: "ZSTD(1)"},
+					{Name: "value", Type: ColumnTypeFloat64, Codec: "Gorilla, ZSTD(1)"},
+					{Name: "labels", Type: ColumnTypeString, Codec: "ZSTD(5)"},
 				},
 				Engine: MergeTree{
 					PartitionBy: "toDate(unix_milli / 1000)",
@@ -36,16 +34,16 @@ var AnalyticsMigrations = []SchemaMigrationRecord{
 				Database: "signoz_analytics",
 				Table:    "distributed_rule_state_history_v0",
 				Columns: []Column{
-					{Name: "rule_id", Type: ColumnTypeString},
-					{Name: "rule_name", Type: ColumnTypeString},
-					{Name: "overall_state", Type: ColumnTypeString},
+					{Name: "rule_id", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
+					{Name: "rule_name", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
+					{Name: "overall_state", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
 					{Name: "overall_state_changed", Type: ColumnTypeBool},
-					{Name: "state", Type: ColumnTypeString},
+					{Name: "state", Type: LowCardinalityColumnType{ElementType: ColumnTypeString}},
 					{Name: "state_changed", Type: ColumnTypeBool},
-					{Name: "unix_milli", Type: ColumnTypeInt64},
-					{Name: "fingerprint", Type: ColumnTypeUInt64},
-					{Name: "value", Type: ColumnTypeFloat64},
-					{Name: "labels", Type: ColumnTypeString},
+					{Name: "unix_milli", Type: ColumnTypeInt64, Codec: "Delta(8), ZSTD(1)"},
+					{Name: "fingerprint", Type: ColumnTypeUInt64, Codec: "ZSTD(1)"},
+					{Name: "value", Type: ColumnTypeFloat64, Codec: "Gorilla, ZSTD(1)"},
+					{Name: "labels", Type: ColumnTypeString, Codec: "ZSTD(5)"},
 				},
 				Engine: Distributed{
 					Database:    "signoz_analytics",
