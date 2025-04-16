7681a37d8a40fc199903c6b2dd12ee1bec6b30bc
aniket <aniket@signoz.io>
2025-04-16 18:29:48 +0530

    feat(metrics-exporter): added test cases

--- pkg/pdatagen/pmetricsgen/data.go
+++ pkg/pdatagen/pmetricsgen/data.go
@@ -1,6 +1,8 @@
 package pmetricsgen
 
 import (
+	"github.com/prometheus/prometheus/model/value"
+	"math"
 	"math/rand/v2"
 	"strconv"
 	"time"
@@ -381,6 +383,380 @@ func GenerateSummaryMetrics(numMetrics, numDataPoints, numPointAttributes, numSc
 			quantileValues := dp.QuantileValues().AppendEmpty()
 			quantileValues.SetValue(float64(j))
 			quantileValues.SetQuantile(float64(j))
+		}
+	}
+	return metrics
+}
+
+func GenerateGaugeNanMetrics(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("system.memory.usage" + strconv.Itoa(i))
+		m.SetUnit("bytes")
+		m.SetDescription("memory usage of the host")
+		dpSlice := m.SetEmptyGauge().DataPoints()
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetDoubleValue(math.NaN())
+			dp.SetFlags(pmetric.DefaultDataPointFlags)
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("gauge.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+		}
+	}
+	return metrics
+}
+
+func GenerateGaugeMetricsWithNoRecordedValueFlag(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("system.memory.usage" + strconv.Itoa(i))
+		m.SetUnit("bytes")
+		m.SetDescription("memory usage of the host")
+		dpSlice := m.SetEmptyGauge().DataPoints()
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+			dp.SetDoubleValue(float64(i))
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("gauge.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+		}
+	}
+	return metrics
+}
+
+func GenerateHistogramMetricsWithNoRecordedValueFlag(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("http.server.duration" + strconv.Itoa(i))
+		m.SetUnit("ms")
+		m.SetDescription("server duration of the http server")
+		dpSlice := m.SetEmptyHistogram().DataPoints()
+		if i%2 == 0 {
+			m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		} else {
+			m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+		}
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+			dp.SetCount(30)
+			dp.SetSum(35)
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("histogram.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.ExplicitBounds().FromRaw([]float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19})
+			dp.BucketCounts().FromRaw([]uint64{1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 12, 1, 1, 1, 1, 1, 1, 1})
+			dp.SetMin(0)
+			dp.SetMax(12)
+		}
+	}
+	return metrics
+}
+
+func GenerateHistogramMetricsWithNanValuesAndNilValues(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("http.server.duration" + strconv.Itoa(i))
+		m.SetUnit("ms")
+		m.SetDescription("server duration of the http server")
+		dpSlice := m.SetEmptyHistogram().DataPoints()
+		if i%2 == 0 {
+			m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		} else {
+			m.Histogram().SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+		}
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+			dp.SetCount(30)
+			dp.SetSum(math.Float64frombits(value.StaleNaN))
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("histogram.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.ExplicitBounds().FromRaw([]float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19})
+			dp.BucketCounts().FromRaw([]uint64{1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 12, 1, 1, 1, 1, 1, 1, 1})
+			dp.SetMin(math.Float64frombits(value.StaleNaN))
+			dp.SetMax(12)
+		}
+	}
+	return metrics
+}
+
+func GenerateSumMetricsWithNoRecordedValue(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("system.cpu.time" + strconv.Itoa(i))
+		m.SetUnit("s")
+		m.SetDescription("cpu time of the host")
+		dpSlice := m.SetEmptySum().DataPoints()
+		if i%2 == 0 {
+			m.Sum().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		} else {
+			m.Sum().SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+		}
+		if i%3 == 0 {
+			m.Sum().SetIsMonotonic(true)
+		} else {
+			m.Sum().SetIsMonotonic(false)
+		}
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetIntValue(int64(i))
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("sum.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+		}
+	}
+	return metrics
+}
+
+func GenerateSummaryMetricsWithNoRecordedValue(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("zk.duration" + strconv.Itoa(i))
+		m.SetUnit("ms")
+		m.SetDescription("This is a summary metrics")
+		slice := m.SetEmptySummary().DataPoints()
+		for j := 0; j < numDataPoints; j++ {
+			dp := slice.AppendEmpty()
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetCount(uint64(j))
+			dp.SetSum(float64(j))
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("summary.attr_"+strconv.Itoa(k), "1")
+			}
+			quantileValues := dp.QuantileValues().AppendEmpty()
+			quantileValues.SetValue(float64(j))
+			quantileValues.SetQuantile(float64(j))
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+		}
+	}
+	return metrics
+}
+
+func GenerateSummaryMetricsWithNan(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("zk.duration" + strconv.Itoa(i))
+		m.SetUnit("ms")
+		m.SetDescription("This is a summary metrics")
+		slice := m.SetEmptySummary().DataPoints()
+		for j := 0; j < numDataPoints; j++ {
+			dp := slice.AppendEmpty()
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetCount(uint64(j))
+			dp.SetSum(float64(j))
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("summary.attr_"+strconv.Itoa(k), "1")
+			}
+			quantileValues := dp.QuantileValues().AppendEmpty()
+			quantileValues.SetValue(math.NaN())
+			quantileValues.SetQuantile(float64(j))
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+		}
+	}
+	return metrics
+}
+
+func GenerateExponentialHistogramMetricsWithNoRecordedValue(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("http.server.duration" + strconv.Itoa(i))
+		m.SetUnit("ms")
+		m.SetDescription("server duration of the http server but in exponential histogram format")
+		dpSlice := m.SetEmptyExponentialHistogram().DataPoints()
+		if i%2 == 0 {
+			m.ExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		} else {
+			m.ExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+		}
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetScale(2)
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetSum(1)
+			dp.SetMin(0)
+			dp.SetMax(1)
+			dp.SetZeroCount(0)
+			dp.SetCount(1)
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("exponential.histogram.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.Negative().SetOffset(1)
+			dp.Negative().BucketCounts().FromRaw([]uint64{0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 11, 1, 1, 1, 1, 10})
+			dp.Positive().SetOffset(1)
+			dp.Positive().BucketCounts().FromRaw([]uint64{0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 11, 1, 1, 1, 1, 10})
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
+		}
+	}
+	return metrics
+}
+
+func GenerateExponentialHistogramMetricsWithNan(numMetrics, numDataPoints, numPointAttributes, numScopeAttributes, numResourceAttributes int) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	rm := metrics.ResourceMetrics().AppendEmpty()
+	for i := 0; i < numResourceAttributes; i++ {
+		rm.Resource().Attributes().PutStr("resource.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	rm.SetSchemaUrl("resource.schema_url")
+	sm := rm.ScopeMetrics().AppendEmpty()
+	sm.SetSchemaUrl("scope.schema_url")
+	for i := 0; i < numScopeAttributes; i++ {
+		sm.Scope().Attributes().PutStr("scope.attr_"+strconv.Itoa(i), "value"+strconv.Itoa(i))
+	}
+	sm.Scope().SetName("go.signoz.io/app/reader")
+	sm.Scope().SetVersion("1.0.0")
+	for i := 0; i < numMetrics; i++ {
+		m := sm.Metrics().AppendEmpty()
+		m.SetName("http.server.duration" + strconv.Itoa(i))
+		m.SetUnit("ms")
+		m.SetDescription("server duration of the http server but in exponential histogram format")
+		dpSlice := m.SetEmptyExponentialHistogram().DataPoints()
+		if i%2 == 0 {
+			m.ExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+		} else {
+			m.ExponentialHistogram().SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+		}
+		for j := 0; j < numDataPoints; j++ {
+			dp := dpSlice.AppendEmpty()
+			dp.SetScale(2)
+			dp.SetStartTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(time.Unix(1727286182+int64(j), 0)))
+			dp.SetSum(math.NaN())
+			dp.SetMin(0)
+			dp.SetMax(math.NaN())
+			dp.SetZeroCount(0)
+			dp.SetCount(1)
+			for k := 0; k < numPointAttributes; k++ {
+				dp.Attributes().PutStr("exponential.histogram.attr_"+strconv.Itoa(k), "1")
+			}
+			dp.Negative().SetOffset(1)
+			dp.Negative().BucketCounts().FromRaw([]uint64{0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 11, 1, 1, 1, 1, 10})
+			dp.Positive().SetOffset(1)
+			dp.Positive().BucketCounts().FromRaw([]uint64{0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 11, 1, 1, 1, 1, 10})
+			dp.SetFlags(dp.Flags().WithNoRecordedValue(true))
 		}
 	}
 	return metrics
