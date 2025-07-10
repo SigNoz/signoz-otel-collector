1a2c9e625138f8bafef3b4c9987ae839445d3afe
Piyush Singariya <piyushsingariya@gmail.com>
2025-07-10 16:01:42 +0530

    test: sequential testing

--- processor/signozlogspipelineprocessor/processor.go
+++ processor/signozlogspipelineprocessor/processor.go
@@ -17,7 +17,6 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
 
 	_ "github.com/SigNoz/signoz-otel-collector/pkg/parser/grok" // ensure grok parser gets registered.
 	"github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/internal/metadata"
@@ -123,25 +122,25 @@ func (p *logsPipelineProcessor) ProcessLogs(ctx context.Context, ld plog.Logs) (
 		}
 	}
 
-	group, groupCtx := errgroup.WithContext(ctx)
+	// group, groupCtx := errgroup.WithContext(ctx)
 	// group.SetLimit(runtime.NumCPU() * 5)
 	for _, batch := range utils.Batch(entries, p.batchSize) {
-		select {
-		case p.limiter <- struct{}{}:
-			group.Go(func() error {
-				defer func() {
-					<-p.limiter
-				}()
-				process(groupCtx, batch)
-				return nil // not returning error to avoid cancelling groupCtx
-			})
-		default:
-			process(ctx, batch)
-		}
+		// select {
+		// case p.limiter <- struct{}{}:
+		// 	group.Go(func() error {
+		// 		defer func() {
+		// 			<-p.limiter
+		// 		}()
+		// 		process(groupCtx, batch)
+		// 		return nil // not returning error to avoid cancelling groupCtx
+		// 	})
+		// default:
+		process(ctx, batch)
+		// }
 	}
 
 	// wait for the group execution
-	_ = group.Wait()
+	// _ = group.Wait()
 
 	p.durationHistogram.Record(ctx,
 		float64(time.Since(start).Seconds()),
