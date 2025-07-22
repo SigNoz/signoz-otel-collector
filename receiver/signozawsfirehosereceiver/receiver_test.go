c0bdd5eaa85e54db9deaf0e67894f6aaebd654c2
Piyush Singariya <piyushsingariya@gmail.com>
2025-07-22 13:01:50 +0530

    fix: tests

--- receiver/signozawsfirehosereceiver/receiver_test.go
+++ receiver/signozawsfirehosereceiver/receiver_test.go
@@ -130,7 +130,7 @@ func TestFirehoseRequest(t *testing.T) {
 		"WithInvalidBody": {
 			body:           "{ test: ",
 			wantStatusCode: http.StatusBadRequest,
-			wantErr:        errors.New("json: cannot unmarshal string into Go value of type signozawsfirehosereceiver.firehoseRequest"),
+			wantErr:        errors.New("invalid character '\"' looking for beginning of value"),
 		},
 		"WithNoRecords": {
 			body:           testFirehoseRequest(testFirehoseRequestID, noRecords),
