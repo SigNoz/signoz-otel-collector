a496bf0f7b71de93d83fa66c865906aa59511cc6
Piyush Singariya <piyushsingariya@gmail.com>
2025-10-28 11:07:55 +0530

    sync: type exporter lint changes

--- exporter/jsontypeexporter/types.go
+++ exporter/jsontypeexporter/types.go
@@ -1,9 +1,5 @@
 package jsontypeexporter
 
-import (
-	"reflect"
-)
-
 const (
 	StringType   = "String"
 	IntType      = "Int"
@@ -16,23 +12,3 @@ const (
 	ArrayString  = "Array(Nullable(String))"
 	ArrayJSON    = "Array(JSON)"
 )
-
-var kindToTypes = map[reflect.Kind]string{
-	reflect.String:  StringType,
-	reflect.Int64:   IntType,
-	reflect.Float64: Float64Type,
-	reflect.Bool:    BooleanType,
-}
-
-var kindToArrayTypes = map[reflect.Kind]string{
-	reflect.String:  ArrayString,
-	reflect.Int64:   ArrayInt,
-	reflect.Int:     ArrayInt,
-	reflect.Int8:    ArrayInt,
-	reflect.Int16:   ArrayInt,
-	reflect.Int32:   ArrayInt,
-	reflect.Float64: ArrayFloat64,
-	reflect.Float32: ArrayFloat64,
-	reflect.Bool:    ArrayBoolean,
-	reflect.Map:     ArrayJSON,
-}
