b12dd3cbf9d17ecba54f5b8a60ba863407ad986b
Vikrant Gupta <vikrant@signoz.io>
2025-11-25 12:31:21 +0530

    feat(migrate): move collector flags to cobra (#721)
    ## Description 
    
    - Move the collector config flags to cobra command line 
    
    Contributes to: https://github.com/SigNoz/platform-pod/issues/1359
    
    ### Usage for feature gates remains unchanged
    ```
    go run main.go --config config.yaml --feature-gates=+pkg.translator.prometheus.NormalizeName
    ```
    
    <img width="1512" height="525" alt="image"
    src="https://github.com/user-attachments/assets/e45990d6-acaa-45f9-95d2-c3cf46c57579"
    />
    
    ### Disabling feature gates
    ```
    go run main.go --config config.yaml --feature-gates=-pkg.translator.prometheus.NormalizeName
    ```
    <img width="1512" height="522" alt="image"
    src="https://github.com/user-attachments/assets/5d0708f4-dc6f-4f84-a8f5-1239f9554950"
    />
    
    
    ### Environment variables within collector config
    ```
    receivers:
      otlp:
        protocols:
          grpc:
          http:
    processors:
      batch:
        send_batch_size: 1000
        timeout: 10s
    extensions:
      zpages: {}
    exporters:
      debug:
    service:
      extensions: [zpages]
      pipelines:
        traces:
          receivers: [otlp]
          processors: [batch]
          exporters: [debug]
        metrics:
          receivers: [otlp]
          processors: [batch]
          exporters: [debug]
      telemetry:
          resource:
              service.instance.id: ${env:INSTANCE_ID}
    ```
    ```
    INSTANCE_ID="abc" go run main.go --config config.yaml --feature-gates=+pkg.translator.prometheus.NormalizeName
    {"level":"info","timestamp":"2025-11-25T10:57:40.823+0530","caller":"service/service.go:69","msg":"Starting service"}
    {"level":"info","timestamp":"2025-11-25T10:57:40.824+0530","caller":"opamp/simple_client.go:26","msg":"Starting simple client","component":"simple-client"}
    {"level":"info","timestamp":"2025-11-25T10:57:40.828+0530","caller":"signozcol/collector.go:102","msg":"Starting collector service"}
    2025-11-25T10:57:40.837+0530    info    service@v0.128.0/service.go:199 Setting up own telemetry...     {"resource": {"service.instance.id": "abc", "service.name": "/Users/vikrantgupta/Library/Caches/go-build/5e/5e76515427bee0b59851cb142b9d3fda09405e4f58fc72a2344a6b5089863354-d/main", "service.version": 
    ```

--- cmd/signozotelcollector/config/config.go
+++ cmd/signozotelcollector/config/config.go
@@ -1,11 +1,30 @@
 package config
 
-import "github.com/spf13/cobra"
+import (
+	signozcolFeatureGate "github.com/SigNoz/signoz-otel-collector/featuregate"
+	"github.com/spf13/cobra"
+	otelcolFeatureGate "go.opentelemetry.io/collector/featuregate"
+)
 
 var (
+	Collector        collector
 	Clickhouse       clickhouse
 	MigrateSyncCheck migrateSyncCheck
 )
+
+type collector struct {
+	Config        string
+	ManagerConfig string
+	CopyPath      string
+}
+
+func (cfg *collector) RegisterFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().StringVar(&cfg.Config, "config", "", "File path for the collector configuration")
+	cmd.PersistentFlags().StringVar(&cfg.ManagerConfig, "manager-config", "", "File path for the agent manager configuration")
+	cmd.PersistentFlags().StringVar(&cfg.CopyPath, "copy-path", "/etc/otel/signozcol-config.yaml", "File path for the copied collector configuration")
+	cmd.PersistentFlags().Var(signozcolFeatureGate.NewFlag(otelcolFeatureGate.GlobalRegistry()), "feature-gates",
+		"Comma-delimited list of feature gate identifiers. Prefix with '-' to disable the feature. '+' or no prefix will enable the feature.")
+}
 
 type clickhouse struct {
 	DSN string
