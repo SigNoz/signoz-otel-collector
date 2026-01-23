444d29747bb1d634369da9631a88137be4cf7997
nikhilmantri0902 <nikhil.mantri1999@gmail.com>
2026-01-23 15:50:58 +0530

    remove not required Queuebatchconfig variables in TestConfig_Validate_Valid

--- exporter/signozclickhousemetrics/config_test.go
+++ exporter/signozclickhousemetrics/config_test.go
@@ -2,11 +2,8 @@ package signozclickhousemetrics
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/config/configoptional"
-	"go.opentelemetry.io/collector/exporter/exporterhelper"
 )
 
 func TestConfig_Validate(t *testing.T) {
@@ -18,15 +15,6 @@ func TestConfig_Validate(t *testing.T) {
 func TestConfig_Validate_Valid(t *testing.T) {
 	cfg := &Config{
 		DSN: "tcp://localhost:9000?database=default",
-		QueueBatchConfig: configoptional.Some(exporterhelper.QueueBatchConfig{
-			QueueSize:    100,
-			NumConsumers: 1,
-			Batch: configoptional.Default(exporterhelper.BatchConfig{
-				FlushTimeout: 200 * time.Millisecond,
-				Sizer:        exporterhelper.RequestSizerTypeItems,
-				MinSize:      8192,
-			}),
-		}),
 	}
 	err := cfg.Validate()
 	require.NoError(t, err)
