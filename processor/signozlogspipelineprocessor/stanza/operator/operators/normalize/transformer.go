1d4233bcd67045b2e821b2752fa15920322faae8
Piyush Singariya <piyushsingariya@gmail.com>
2026-02-16 15:52:51 +0530

    feat: deprecate body String column

--- processor/signozlogspipelineprocessor/stanza/operator/operators/normalize/transformer.go
+++ processor/signozlogspipelineprocessor/stanza/operator/operators/normalize/transformer.go
@@ -2,15 +2,21 @@ package json
 
 import (
 	"context"
+	"sort"
 	"strings"
 
+	signozstanzaentry "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/entry"
 	signozstanzahelper "github.com/SigNoz/signoz-otel-collector/processor/signozlogspipelineprocessor/stanza/operator/helper"
 	"github.com/SigNoz/signoz-otel-collector/utils"
 	"github.com/bytedance/sonic"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/entry"
 )
 
-// var msgCompatibleFields = []string{"msg"}
+const (
+	MessageField = "message"
+)
+
+var msgCompatibleFields = []string{"log", "msg"}
 
 type Processor struct {
 	signozstanzahelper.TransformerOperator
@@ -31,17 +37,7 @@ func (p *Processor) transform(entry *entry.Entry) error {
 	parsedValue := make(map[string]any)
 	switch v := entry.Body.(type) {
 	case string:
-		// Unquote JSON strings if possible
-		unquoted := utils.Unquote(v)
-		if !(strings.HasPrefix(unquoted, "{") && strings.HasSuffix(unquoted, "}")) {
-			return nil
-		}
-		dec := p.Config.Froze().NewDecoder(strings.NewReader(unquoted))
-		err := dec.Decode(&parsedValue)
-		if err != nil { // failed to marshal as JSON; return as is
-			return nil
-		}
-
+		parsedValue = p.processTextLogs(v)
 	// no need to cover other map types; check comment https://github.com/SigNoz/signoz-otel-collector/pull/584#discussion_r2042020882
 	case map[string]any:
 		parsedValue = v
@@ -52,30 +48,45 @@ func (p *Processor) transform(entry *entry.Entry) error {
 	// set parsed value to body
 	entry.Body = parsedValue
 
-	// TODO: re-enable in PipelinesV2
-	// messageField := signozstanzaentry.NewBodyField("message")
-	// // add first found msg compatible field to body
-	// for _, fieldName := range msgCompatibleFields {
-	// 	field := signozstanzaentry.NewBodyField(fieldName)
-	// 	val, ok := entry.Get(field)
-	// 	if !ok {
-	// 		continue
-	// 	}
-	// 	strValue, ok := val.(string)
-	// 	if !ok {
-	// 		continue
-	// 	}
-	// 	err := entry.Set(messageField, strValue)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	entry.Delete(field)
-	// 	break
-	// }
+	messageField := signozstanzaentry.NewBodyField("message")
+	// add first found msg compatible field to body
+	for _, fieldName := range msgCompatibleFields {
+		field := signozstanzaentry.NewBodyField(fieldName)
+		val, ok := entry.Get(field)
+		if !ok {
+			continue
+		}
+		strValue, ok := val.(string)
+		if !ok {
+			continue
+		}
+		err := entry.Set(messageField, strValue)
+		if err != nil {
+			return err
+		}
+		entry.Delete(field)
+		break
+	}
 
 	return nil
 }
 
-// func init() {
-// 	sort.Strings(msgCompatibleFields)
-// }
+func (p *Processor) processTextLogs(str string) map[string]any {
+	output := make(map[string]any)
+	// Unquote JSON strings if possible
+	unquoted := utils.Unquote(str)
+	// if string can be a valid JSON
+	if strings.HasPrefix(unquoted, "{") && strings.HasSuffix(unquoted, "}") {
+		dec := p.Config.Froze().NewDecoder(strings.NewReader(unquoted))
+		err := dec.Decode(&output)
+		if err == nil { // successfully decoded as JSON; return as is
+			return output
+		}
+	}
+	output[MessageField] = str
+	return output
+}
+
+func init() {
+	sort.Strings(msgCompatibleFields)
+}
